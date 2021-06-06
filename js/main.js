f0b7ec6ff16a679088b9c27e15f8e92d8fa6716f
dankml <85433914+dankml@users.noreply.github.com>
2021-06-06 10:56:07 +0200

    i uploaded my file = first time

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var getUsername = prompt("Put yo name Beach");
+
+console.log(getUsername);
+
+document.getElementById("a").innerText = getUsername;
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
